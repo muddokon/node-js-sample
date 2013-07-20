64e108582c4bb5dd635bcbd4f424677c421b4658
Sebastian <muddokon@hotmail.es>
2013-07-20 09:55:44 -0500

    Buffer2 en web.js

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
 var express = require('express');
+var fs = require('fs');
+
+var buf = fs.readFileSync('index.html', function (err, data) {
+  if (err) throw err;
+  console.log(data);
+});
+
+
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    response.send(buf.toString('utf-8', 0, buf.length));
+  //response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
